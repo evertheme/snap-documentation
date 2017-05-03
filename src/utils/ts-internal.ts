261ac2fd9e413d28f0ec5b4a263397d3b7bad4f9
Kevin Crawford <github@evertheme.com>
2017-05-02 19:45:01 -0600

    delete older version

--- src/utils/ts-internal.ts
+++
@@ -1,7 +0,0 @@
-import * as ts from "typescript";
-const tsany = ts as any;
-
-// https://github.com/Microsoft/TypeScript/blob/2.1/src/compiler/utilities.ts#L1507
-export function getJSDocs(node: ts.Node) {
-  return tsany.getJSDocs.apply(this, arguments);
-}
--- test/src/sample-files/foo.directive.ts
+++
@@ -1,4 +0,0 @@
-import { Directive } from '@angular2/core';
-
-@Directive({ selector: '[app-foo]' })
-export class FooDirective { }
